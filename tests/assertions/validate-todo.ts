5abe658cc292aa2a9bec106079558745348c4d08
Daviduy71325 <daviduy71325@gmail.com>
2023-09-09 15:37:09 +0800

    Fixed assertions

--- tests/assertions/validate-todo.ts
+++ tests/assertions/validate-todo.ts
@@ -4,6 +4,7 @@ export default (todo: ITodo): void => {
     expect(todo).toHaveProperty('id')
     expect(todo).toHaveProperty('name')
     expect(todo).toHaveProperty('when_done')
+    expect(todo).toHaveProperty('deleted_at')
     expect(todo).toHaveProperty('created_at')
     expect(todo).toHaveProperty('updated_at')
 }
